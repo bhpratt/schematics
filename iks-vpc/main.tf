9e431a6199c63f3816d95ced398c692995c3b601
Baker Pratt <bpratt@us.ibm.com>
2020-08-10 14:30:05 -0400

    added wait_till flag for vpc

--- iks-vpc/main.tf
+++ iks-vpc/main.tf
@@ -65,6 +65,8 @@ resource "ibm_container_vpc_cluster" "cluster" {
   flavor            = var.flavor
   worker_count      = var.worker_count
   resource_group_id = data.ibm_resource_group.resource_group.id
+  # Lets the user start working with the cluster as soon as a node is available
+  wait_till         = OneWorkerNodeReady
 
   zones {
     subnet_id = ibm_is_subnet.subnet1.id
