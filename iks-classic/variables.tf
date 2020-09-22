12ceb08a12bec6dd93c84e35acd430a1051a9f03
bpratt <bpratt@us.ibm.com>
2020-09-22 13:04:36 -0400

    add base files

---
+++ iks-classic/variables.tf
@@ -0,0 +1,28 @@
+
+variable "machine_type" {
+  default = "b2c.4x16"
+}
+
+variable "region" {
+  default = "us-east"
+}
+
+variable "resource_group" {
+  default = "default"
+}
+
+variable "name" {
+  default = "iks-cluster"
+}
+
+variable "public_vlan_id" {}
+
+variable "private_vlan_id" {}
+
+variable "datacenter" {
+  default = "wdc07"
+}
+
+variable "default_pool_size" {
+  default = "3"
+}
