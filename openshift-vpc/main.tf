ed913e19944fd35c0237525c979799283d129a2b
bpratt <bpratt@us.ibm.com>
2021-04-22 10:39:59 -0400

    removed security groups for 4.6

--- openshift-vpc/main.tf
+++ openshift-vpc/main.tf
@@ -24,23 +24,23 @@ data "ibm_resource_group" "resource_group" {
 
 # required inbound connectivity for VPC LB traffic to worker nodes for openshift 4.4 clusters and earlier
 # https://cloud.ibm.com/docs/openshift?topic=openshift-clusters#clusters_vpcg2
-resource "ibm_is_security_group_rule" "security_group_rule_default_tcp" {
-    group = ibm_is_vpc.vpc1.default_security_group
-    direction = "inbound"
-    tcp {
-      port_min = 30000
-      port_max = 32767
-    }
- }
+# resource "ibm_is_security_group_rule" "security_group_rule_default_tcp" {
+#     group = ibm_is_vpc.vpc1.default_security_group
+#     direction = "inbound"
+#     tcp {
+#       port_min = 30000
+#       port_max = 32767
+#     }
+#  }
 
- resource "ibm_is_security_group_rule" "security_group_rule_default_udp" {
-    group = ibm_is_vpc.vpc1.default_security_group
-    direction = "inbound"
-    udp {
-      port_min = 30000
-      port_max = 32767
-    }
- }
+#  resource "ibm_is_security_group_rule" "security_group_rule_default_udp" {
+#     group = ibm_is_vpc.vpc1.default_security_group
+#     direction = "inbound"
+#     udp {
+#       port_min = 30000
+#       port_max = 32767
+#     }
+#  }
 
   # Include public gateway for connectivity outside of VPC
  resource "ibm_is_public_gateway" "gateway_subnet1" {
@@ -94,13 +94,13 @@ resource "ibm_container_vpc_cluster" "cluster" {
   }
 
   # uncomment to create a multizone cluster
-  # zones {
-  #   subnet_id = ibm_is_subnet.subnet2.id
-  #   name      = "${var.region}-2"
-  # }
+  zones {
+    subnet_id = ibm_is_subnet.subnet2.id
+    name      = "${var.region}-2"
+  }
 
-  # zones {
-  #   subnet_id = ibm_is_subnet.subnet3.id
-  #   name      = "${var.region}-3"
-  # }
+  zones {
+    subnet_id = ibm_is_subnet.subnet3.id
+    name      = "${var.region}-3"
+  }
 }
