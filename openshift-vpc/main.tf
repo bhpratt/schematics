fec920c0f3238c89d7c25be01d407959527fd564
bpratt <bpratt@us.ibm.com>
2021-04-22 10:42:09 -0400

    add wait_till flag

--- openshift-vpc/main.tf
+++ openshift-vpc/main.tf
@@ -87,6 +87,8 @@ resource "ibm_container_vpc_cluster" "cluster" {
   worker_count      = var.worker_count
   resource_group_id = data.ibm_resource_group.resource_group.id
   cos_instance_crn  = ibm_resource_instance.cos_instance.id
+  wait_till         = "OneWorkerNodeReady"
+
 
   zones {
     subnet_id = ibm_is_subnet.subnet1.id
