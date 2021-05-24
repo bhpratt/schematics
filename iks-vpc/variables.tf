171eed1f5cc5b6c4058d2c349dfdce4bddd1cb6a
bpratt <bpratt@us.ibm.com>
2021-05-24 13:47:26 -0400

    update variables to remove k8s variables

--- iks-vpc/variables.tf
+++ iks-vpc/variables.tf
@@ -38,32 +38,3 @@ variable "delete_storage" {
   description = "Choose whether you want storage created by the cluster to be deleted at the same time as the cluster"
   default = true
 }
-
-
-variable "namespace" {
-  type = string
-  description = "Namespace to host the deployed K8s resources"
-  default = "cli-tool"
-}
-
-variable "registry_server" {
-  type = string
-  description = "Container image registry region"
-  default = "us.icr.io"
-}
-
-variable "registry_username" {
-  type = string
-  description = "User name for registry"
-  default = "iamapikey"
-}
-
-variable "ibmcloud_api_key" {}
-
-variable "login_key" {}
-
-variable "staging_key" {}
-
-variable "registry_key" {}
-
-variable "slack_webhook_url" {}
