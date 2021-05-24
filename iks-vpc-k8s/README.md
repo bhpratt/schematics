c8e49fecd635073710ba154665bbce0d3d2498a5
bpratt <bpratt@us.ibm.com>
2021-05-24 13:40:38 -0400

    name change directory

--- iks-vpc-k8s/README.md
+++
@@ -1,5 +0,0 @@
-# IKS VPC Gen 2 cluster
-
-Run a VPC Kubernetes cluster on IBM Cloud and automatically provision Kubernetes resources. To be used with Terraform or IBM Cloud Schematics. Allows for a quick build and teardown of a Kubernetes and VPC environment.
-
-source: [IBM Cloud Terraform provider](https://github.com/IBM-Cloud/terraform-provider-ibm/tree/master/examples/ibm-cluster/vpc-gen2-cluster)
--- iks-vpc/variables.tf
+++ iks-vpc/variables.tf
@@ -1,29 +1,69 @@
 
 variable "flavor" {
+  type = string
   default = "bx2.2x8"
 }
 variable "worker_count" {
-  default = "2"
+  type = number
+  default = 2
 }
 
 variable "region" {
+  type = string
   default = "us-east"
 }
 
 variable "resource_group" {
+  type = string
   default = "default"
 }
 
 variable "name" {
-  default = "cluster"
+  type = string
+  default = "iks-vpc-k8s"
 }
 
 variable "vpc_name" {
-  default = "vpz"
+  type = string
+  default = "iks-vpc-k8s"
 }
 
 variable "classic_access" {
+  type = string
   default = "false"
 }
 
+variable "delete_storage" {
+  type = bool
+  description = "Choose whether you want storage created by the cluster to be deleted at the same time as the cluster"
+  default = true
+}
+
+
+variable "namespace" {
+  type = string
+  description = "Namespace to host the deployed K8s resources"
+  default = "cli-tool"
+}
+
+variable "registry_server" {
+  type = string
+  description = "Container image registry region"
+  default = "us.icr.io"
+}
+
+variable "registry_username" {
+  type = string
+  description = "User name for registry"
+  default = "iamapikey"
+}
+
 variable "ibmcloud_api_key" {}
+
+variable "login_key" {}
+
+variable "staging_key" {}
+
+variable "registry_key" {}
+
+variable "slack_webhook_url" {}
