ac6889f5fd9fca929c2ae97a7d2fd0aa8142f79d
bpratt <bpratt@us.ibm.com>
2021-05-24 13:46:39 -0400

    removing kubernetes resources

--- iks-vpc/main.tf
+++ iks-vpc/main.tf
@@ -14,16 +14,6 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-# review this doc: https://cloud.ibm.com/docs/terraform?topic=terraform-container-data-sources#container-cluster-config-sample
-provider "kubernetes" {
-  # config_path = data.ibm_container_cluster_config.clusterConfig.config_file_path
-  # load_config_file       = "false"
-  host                   = data.ibm_container_cluster_config.clusterConfig.host
-  token                  = data.ibm_container_cluster_config.clusterConfig.token
-  cluster_ca_certificate = data.ibm_container_cluster_config.clusterConfig.ca_certificate
-}
-# review example: https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/data-sources/container_cluster_config
-
 # name of VPC
 resource "ibm_is_vpc" "vpc1" {
   name = var.vpc_name
@@ -35,26 +25,6 @@ resource "ibm_is_vpc" "vpc1" {
 data "ibm_resource_group" "resource_group" {
   name = var.resource_group
 }
-
-# required inbound connectivity for VPC LB traffic to worker nodes on 1.18 and earlier
-# https://cloud.ibm.com/docs/containers?topic=containers-clusters#clusters_vpcg2
-# resource "ibm_is_security_group_rule" "security_group_rule_default_tcp" {
-#     group = ibm_is_vpc.vpc1.default_security_group
-#     direction = "inbound"
-#     tcp {
-#       port_min = 30000
-#       port_max = 32767
-#     }
-#  }
-
-#  resource "ibm_is_security_group_rule" "security_group_rule_default_udp" {
-#     group = ibm_is_vpc.vpc1.default_security_group
-#     direction = "inbound"
-#     udp {
-#       port_min = 30000
-#       port_max = 32767
-#     }
-#  }
 
  # Include public gateway for connectivity outside of VPC
  resource "ibm_is_public_gateway" "gateway_subnet1" {
@@ -117,86 +87,4 @@ resource "ibm_container_vpc_cluster" "cluster" {
   #   subnet_id = ibm_is_subnet.subnet3.id
   #   name      = "${var.region}-3"
   # }
-}
-
-# downloads config so Schematics can deploy Kubernetes resources
-data "ibm_container_cluster_config" "clusterConfig" {
-
-  cluster_name_id = ibm_container_vpc_cluster.cluster.name
-  # config_dir = "/tmp"
-}
-
-# create namespace for cronjob
-resource "kubernetes_namespace" "newNamespace" {
-  metadata {
-    name = var.namespace
-  }
- depends_on = [data.ibm_container_cluster_config.clusterConfig]
-}
-
-# create imagepullsecret for cronjob
-resource "kubernetes_secret" "imagePullSecret" {
-  depends_on = [kubernetes_namespace.newNamespace]
-  metadata {
-    name = "cli-tool-pull-secret"
-    namespace = var.namespace
-  }
-  data = {
-    ".dockerconfigjson" = templatefile("${path.module}/config.json", { registry-server = "var.registry_server", registry-username = "var.registry_username", login-key = "var.registry_key", auth = "${base64encode("var.registry_username:var.registry_key")}" })
-}
-
-  type = "kubernetes.io/dockerconfigjson"
-}
-
-# cluster cronjob deployment
-resource "kubernetes_cron_job" "cliTool" {
-  depends_on = [kubernetes_namespace.newNamespace]
-  metadata {
-    name = "cli-tool"
-    namespace = "cli-tool"
-  }
-  spec {
-    concurrency_policy            = "Replace"
-    failed_jobs_history_limit     = 5
-    schedule                      = "0 15 * * 1-5"
-    starting_deadline_seconds     = 10
-    successful_jobs_history_limit = 5
-    job_template {
-      metadata {}
-      spec {
-        template {
-          metadata {}
-          spec {
-            container {
-              name    = "cli-tool"
-              image   = "us.icr.io/cli-tool/ibmcloud-clis:latest"
-              command = ["/bin/sh", "-c", "./build.sh"]
-              env {
-                #key used by build.sh to log in, build image. changed from ibmcloud_cli_key
-                name = "API_KEY"
-                value = var.registry_key
-              }
-              env {
-                #key used for gobx login
-                name = "LOGIN"
-                value = var.login_key
-              }
-              env {
-                #key used for gobx-staging
-                name = "LOGIN_STAGING"
-                value = var.staging_key
-              }
-              env {
-                name = "SLACK_WEBHOOK_URL"
-                value = var.slack_webhook_url
-              }
-            }
-            image_pull_secrets {
-              name = "cli-tool-pull-secret"
-            }
-          }
-        }
-      }
-    }
-  }
 }
