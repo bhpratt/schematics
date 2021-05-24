9a1b1ff5520fcbaf74d18f0be350ac2f4bb224e8
bpratt <bpratt@us.ibm.com>
2021-05-24 14:13:43 -0400

    remove cluster variables

--- iks-k8s/variables.tf
+++ iks-k8s/variables.tf
@@ -1,44 +1,8 @@
 
-variable "flavor" {
-  type = string
-  default = "bx2.2x8"
-}
-variable "worker_count" {
-  type = number
-  default = 2
-}
-
 variable "region" {
   type = string
   default = "us-east"
 }
-
-variable "resource_group" {
-  type = string
-  default = "default"
-}
-
-variable "name" {
-  type = string
-  default = "iks-vpc-k8s"
-}
-
-variable "vpc_name" {
-  type = string
-  default = "iks-vpc-k8s"
-}
-
-variable "classic_access" {
-  type = string
-  default = "false"
-}
-
-variable "delete_storage" {
-  type = bool
-  description = "Choose whether you want storage created by the cluster to be deleted at the same time as the cluster"
-  default = true
-}
-
 
 variable "namespace" {
   type = string
