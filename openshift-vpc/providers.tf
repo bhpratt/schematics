d304ec10d459623c9152e9adacf6741e8ec6e8e5
bpratt <bpratt@us.ibm.com>
2021-05-27 14:32:47 -0400

    removed unneeded api key var

--- openshift-vpc/providers.tf
+++ openshift-vpc/providers.tf
@@ -19,7 +19,6 @@ terraform {
 
 provider "ibm" {
   region = var.region
-  ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 ##############################################################################
