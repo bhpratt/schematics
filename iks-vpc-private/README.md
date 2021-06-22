f6aaef6cb2116d2077cdb03bb414683926a28f1c
bpratt <bpratt@us.ibm.com>
2021-06-22 16:01:27 -0400

    update values for private cluster

---
+++ iks-vpc-private/README.md
@@ -0,0 +1,5 @@
+# IKS VPC cluster
+
+Run a private VPC Kubernetes cluster on IBM Cloud and automatically provision Kubernetes resources. To be used with Terraform or IBM Cloud Schematics. Allows for a quick build and teardown of a Kubernetes and VPC environment.
+
+source: [IBM Cloud Terraform provider](https://github.com/IBM-Cloud/terraform-provider-ibm/tree/master/examples/ibm-cluster/vpc-gen2-cluster)
---
+++ iks-vpc-private/variables.tf
@@ -0,0 +1,67 @@
+
+variable "flavor" {
+  type = string
+  default = "bx2.2x8"
+}
+variable "worker_count" {
+  type = number
+  default = 2
+}
+
+variable "region" {
+  type = string
+  default = "us-east"
+}
+
+variable "resource_group" {
+  type = string
+  default = "default"
+}
+
+variable "name" {
+  type = string
+  default = "iks-vpc-private"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "iks-vpc-private"
+}
+
+variable "kube_version" {
+  type = string
+  default = null
+}
+
+variable "patch_version" {
+  type = string
+  default = null
+}
+
+variable "update_all_workers" {
+  type = bool
+  default = false
+}
+
+variable "classic_access" {
+  type = string
+  default = "false"
+}
+
+variable "delete_storage" {
+  type = bool
+  description = "Choose whether you want storage created by the cluster to be deleted at the same time as the cluster"
+  default = true
+}
+
+variable "ibmcloud_api_key" {}
+
+variable "public_service_endpoint" {
+  type = bool
+  default = false
+}
+
+variable "private_service_endpoint" {
+  type = bool
+  default = true
+}
