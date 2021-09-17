0af0d10704e34809134a8a3d3ec55232cc2e226b
bpratt <bpratt@us.ibm.com>
2021-09-17 14:45:48 -0400

    add back in api key variable

--- iks-vpc/providers.tf
+++ iks-vpc/providers.tf
@@ -20,7 +20,7 @@ terraform {
 
 provider "ibm" {
   region = var.region
-  # ibmcloud_api_key = var.ibmcloud_api_key
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 ##############################################################################
