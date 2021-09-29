ee12ec70f4b706b78dc7380409d6cbe964d062ca
bpratt <bpratt@us.ibm.com>
2021-09-29 17:03:23 -0400

    updated count

--- openshift-vpc/main.tf
+++ openshift-vpc/main.tf
@@ -79,10 +79,11 @@ resource "ibm_is_subnet" "subnet1" {
 
 # ROKS cluster. Single zone.
 resource "ibm_container_vpc_cluster" "cluster" {
+  count = length(data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions) - 2
   name              = var.name
   vpc_id            = ibm_is_vpc.vpc1.id
   flavor            = var.flavor
-  kube_version      = (var.kube_version != null ? var.kube_version : data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions[2])
+  kube_version      = (var.kube_version != null ? var.kube_version : data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions[count])
   worker_count      = var.worker_count
   disable_public_service_endpoint = var.public_service_endpoint_disabled
   resource_group_id = data.ibm_resource_group.resource_group.id
