c6ed2dd1e900d138061795a2060f189e7b3af426
bpratt <bpratt@us.ibm.com>
2021-10-19 15:16:06 -0400

    remove unneeded security group rules

--- openshift-vpc/main.tf
+++ openshift-vpc/main.tf
@@ -19,27 +19,6 @@ data "ibm_resource_group" "resource_group" {
 # get the list of available cluster versions in IBM Cloud
 data "ibm_container_cluster_versions" "cluster_versions" {
 }
-
-# required inbound connectivity for VPC LB traffic to worker nodes for openshift 4.4 clusters and earlier
-# https://cloud.ibm.com/docs/openshift?topic=openshift-clusters#clusters_vpcg2
-# resource "ibm_is_security_group_rule" "security_group_rule_default_tcp" {
-#     group = ibm_is_vpc.vpc1.default_security_group
-#     direction = "inbound"
-#     tcp {
-#       port_min = 30000
-#       port_max = 32767
-#     }
-#  }
-
-#  resource "ibm_is_security_group_rule" "security_group_rule_default_udp" {
-#     group = ibm_is_vpc.vpc1.default_security_group
-#     direction = "inbound"
-#     udp {
-#       port_min = 30000
-#       port_max = 32767
-#     }
-#  }
-
   # Include public gateway for connectivity outside of VPC
  resource "ibm_is_public_gateway" "gateway_subnet1" {
     name       = "vpc-gen2-roks-gateway"
@@ -51,6 +30,29 @@ data "ibm_container_cluster_versions" "cluster_versions" {
         create = "90m"
     }
 }
+
+# uncomment to create multizone cluster w/3 public gateways
+#  resource "ibm_is_public_gateway" "gateway_subnet2" {
+#     name       = "vpc-gen2-roks-gateway"
+#     vpc        = ibm_is_vpc.vpc1.id
+#     zone       = "${var.region}-2"
+
+#     //User can configure timeouts
+#     timeouts {
+#         create = "90m"
+#     }
+# }
+
+#  resource "ibm_is_public_gateway" "gateway_subnet3" {
+#     name       = "vpc-gen2-roks-gateway"
+#     vpc        = ibm_is_vpc.vpc1.id
+#     zone       = "${var.region}-3"
+
+#     //User can configure timeouts
+#     timeouts {
+#         create = "90m"
+#     }
+# }
 
 # VPC subnets. Uses default CIDR range
 resource "ibm_is_subnet" "subnet1" {
@@ -77,14 +79,11 @@ resource "ibm_is_subnet" "subnet1" {
 #   total_ipv4_address_count = 256
 # }
 
-# ROKS cluster. Single zone.
+# ROKS cluster. Single zone. Kube version by default will take the 3rd in the list of the valid openshift versions given in the output of `ibmcloud oc versions`
 resource "ibm_container_vpc_cluster" "cluster" {
-  //*TO DO*: figure out how to make this container_index work ? maybe locals? https://www.terraform.io/docs/language/values/locals.html#declaring-a-local-value
-  #container_index = length(data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions) - 2
   name                            = var.name
   vpc_id                          = ibm_is_vpc.vpc1.id
   flavor                          = var.flavor
-  #*TO DO* need to tag version with _openshift at the end: https://stackoverflow.com/questions/63443283/how-to-concatenate-a-variable-and-a-string-in-terraform
   kube_version                    = (var.kube_version != null ? var.kube_version : "${data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions[2]}_openshift")
   worker_count                    = var.worker_count
   disable_public_service_endpoint = var.public_service_endpoint_disabled
