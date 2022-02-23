fc8bb83b3993764ad1bac653c33e87694d47083a
bhpratt <bhpratt@ncsu.edu>
2022-02-23 15:08:48 -0500

    added multiaz support

--- iks-classic/main.tf
+++ iks-classic/main.tf
@@ -27,6 +27,24 @@ resource "ibm_container_cluster" "cluster" {
   private_vlan_id   = var.private_vlan_id
   hardware          = "shared"
   default_pool_size = var.default_pool_size
-  private_service_endpoint = "true"
-  public_service_endpoint  = "true"
+  private_service_endpoint = var.private_service_endpoint_enabled
+  public_service_endpoint  = var.public_service_endpoint_enabled
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "zone2" {
+  cluster           = ibm_container_cluster.cluster.id
+  worker_pool       = ibm_container_cluster.cluster.worker_pools.0.id
+  zone              = var.datacenter_zone2
+  private_vlan_id   = var.private_vlan_id_zone2
+  public_vlan_id    = var.public_vlan_id_zone2
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "zone3" {
+  cluster           = ibm_container_cluster.cluster.id
+  worker_pool       = ibm_container_cluster.cluster.worker_pools.0.id
+  zone              = var.datacenter_zone3
+  private_vlan_id   = var.private_vlan_id_zone3
+  public_vlan_id    = var.private_vlan_id_zone3
+  resource_group_id = data.ibm_resource_group.resource_group.id
 }
--- iks-classic/variables.tf
+++ iks-classic/variables.tf
@@ -19,10 +19,34 @@ variable "public_vlan_id" {}
 
 variable "private_vlan_id" {}
 
+variable "public_vlan_id_zone2" {}
+
+variable "private_vlan_id_zone2" {}
+
+variable "public_vlan_id_zone3" {}
+
+variable "private_vlan_id_zone3" {}
+
 variable "datacenter" {
   default = "wdc07"
 }
 
+variable "datacenter_zone2" {
+  default = "wdc04"
+}
+
+variable "datacenter_zone3" {
+  default = "wdc06"
+}
+
 variable "default_pool_size" {
   default = "3"
+}
+
+variable "public_service_endpoint_enabled" {
+  default = true
+}
+
+variable private_service_endpoint_enabled {
+  default = false
 }
