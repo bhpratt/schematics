bd7de91971e4cc0fcddcfdcad99a753ad0e29efe
bhpratt <bhpratt@ncsu.edu>
2022-04-07 17:45:10 -0400

    revert changes

--- openshift-vpc/main.tf
+++ openshift-vpc/main.tf
@@ -3,7 +3,7 @@ resource "ibm_resource_instance" "cos_instance" {
   name     = var.service_instance_name
   service  = var.service_offering
   plan     = var.plan
-  location = "us-south"
+  location = "global"
 }
 
 # name of VPC
