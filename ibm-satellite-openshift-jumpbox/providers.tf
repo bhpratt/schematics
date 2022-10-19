9a4c3bb323e8dee982de9f1c5cabf6a04a01ddf5
bhpratt <bhpratt@ncsu.edu>
2022-10-19 09:44:18 -0400

    roll back version

--- ibm-satellite-openshift-jumpbox/providers.tf
+++ ibm-satellite-openshift-jumpbox/providers.tf
@@ -6,9 +6,9 @@ terraform {
      required_providers {
         ibm = {
            source = "IBM-Cloud/ibm"
-          #  version = ">= 1.45"
-          #  version = "1.1.0"
-            version = "1.47.0-beta2"
+           version = "1.45"
+          #  version = "1.2.0"
+            # version = "1.47.0-beta2"
            }
       }
   required_version = ">= 0.13"
