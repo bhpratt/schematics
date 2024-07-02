bfd6bf6d90c4026c8b15a1fc601d91081f54d0a2
Baker Pratt <bpratt@us.ibm.com>
2024-07-02 10:35:47 -0400

    update files and versions for create vlans directly

--- iks-classic/main.tf
+++ iks-classic/main.tf
@@ -1,20 +1,10 @@
-terraform {
-     required_providers {
-        ibm = {
-           source = "IBM-Cloud/ibm"
-           }
-      }
-  required_version = ">= 0.12"
-}
-
-# specifies gen2 and region for VPC/IKS resources
-provider "ibm" {
-  region = var.region
-}
-
 # name of resource group
 data "ibm_resource_group" "resource_group" {
   name = var.resource_group
+}
+
+# List of available cluster versions in IBM Cloud
+data "ibm_container_cluster_versions" "cluster_versions" {
 }
 
 # IKS cluster. Single zone.
@@ -23,28 +13,24 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   resource_group_id = data.ibm_resource_group.resource_group.id
   datacenter        = var.datacenter
-  public_vlan_id    = var.public_vlan_id
-  private_vlan_id   = var.private_vlan_id
+  kube_version      = (var.kube_version != null ? var.kube_version : data.ibm_container_cluster_versions.cluster_versions.default_kube_version)
+  public_vlan_id    = ibm_network_vlan.public_vlan.id
+  private_vlan_id   = ibm_network_vlan.private_vlan.id
   hardware          = "shared"
   default_pool_size = var.default_pool_size
   private_service_endpoint = var.private_service_endpoint_enabled
   public_service_endpoint  = var.public_service_endpoint_enabled
+  wait_till = "OneWorkerNodeReady"
 }
 
-# resource "ibm_container_worker_pool_zone_attachment" "zone2" {
-#   cluster           = ibm_container_cluster.cluster.id
-#   worker_pool       = ibm_container_cluster.cluster.worker_pools.0.id
-#   zone              = var.datacenter_zone2
-#   private_vlan_id   = var.private_vlan_id_zone2
-#   public_vlan_id    = var.public_vlan_id_zone2
-#   resource_group_id = data.ibm_resource_group.resource_group.id
-# }
+resource "ibm_network_vlan" "public_vlan" {
+  name            = "${var.name}-public"
+  datacenter      = var.datacenter
+  type            = "PUBLIC"
+}
 
-# resource "ibm_container_worker_pool_zone_attachment" "zone3" {
-#   cluster           = ibm_container_cluster.cluster.id
-#   worker_pool       = ibm_container_cluster.cluster.worker_pools.0.id
-#   zone              = var.datacenter_zone3
-#   private_vlan_id   = var.private_vlan_id_zone3
-#   public_vlan_id    = var.private_vlan_id_zone3
-#   resource_group_id = data.ibm_resource_group.resource_group.id
-# }
+resource "ibm_network_vlan" "private_vlan" {
+  name            = "${var.name}-private"
+  datacenter      = var.datacenter
+  type            = "PRIVATE"
+}
--- iks-classic/variables.tf
+++ iks-classic/variables.tf
@@ -1,6 +1,7 @@
+variable "ibmcloud_api_key" {}
 
 variable "machine_type" {
-  default = "b2c.4x16"
+  default = "b3c.4x16"
 }
 
 variable "region" {
@@ -12,47 +13,20 @@ variable "resource_group" {
 }
 
 variable "name" {
-  default = "iks-cluster"
+  default = "iks-classic"
 }
 
-variable "public_vlan_id" {
+variable "kube_version" {
   default = null
 }
 
-variable "private_vlan_id" {
-  default = null
-}
-
-variable "public_vlan_id_zone2" {
-  default = null
-}
-
-variable "private_vlan_id_zone2" {
-  default = null
-}
-
-variable "public_vlan_id_zone3" {
-  default = null
-}
-
-variable "private_vlan_id_zone3" {
-  default = null
-}
 
 variable "datacenter" {
   default = "wdc07"
 }
 
-variable "datacenter_zone2" {
-  default = "wdc04"
-}
-
-variable "datacenter_zone3" {
-  default = "wdc06"
-}
-
 variable "default_pool_size" {
-  default = "3"
+  default = "2"
 }
 
 variable "public_service_endpoint_enabled" {
